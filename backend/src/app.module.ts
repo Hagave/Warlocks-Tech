62d52aa024d501a90ce5bca9faed3189194f1016
Hagave <hagavepro@gmail.com>
2025-02-12 15:58:03 -0300

    first: first-commit

---
+++ backend/src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { UsersModule } from './users/users.module';
+import { NotesModule } from './notes/notes.module';
+import { AuthModule } from './auth/auth.module';
+
+@Module({
+  imports: [UsersModule, NotesModule, AuthModule],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
